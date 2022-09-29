fde4789cb4d994ac51b502e5b1435491c805f688
lilyLis7216 <silverfish_shion@yahoo.co.jp>
2022-09-29 17:34:52 +0900

    Playerクラスの追加

---
+++ Player.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "BaseObject.h"
+
+class Player : public BaseObject
+{
+public:
+    Player();
+    ~Player();
+
+    void Update() override;
+    void Draw()   override;
+
+private:
+    void Move();
+};
