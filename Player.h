91de19b17218f2c86057e20178f6dba4c089317e
408-12 <>
2022-10-03 12:24:58 +0900

    Playerクラスの修正

--- Player.h
+++ Player.h
@@ -7,12 +7,23 @@ public:
     Player();
     ~Player();
 
-    void Update() override;
+    void Update(float deltaTime) override;
     void Draw()   override;
 
 private:
-    void Move();
+    void Move(float deltaTime);
+
+    void AdjustPos();
+
+    //! ‰¡•‚Ì”¼•ª
+    float halfScaleX;
+    //! c•‚Ì”¼•ª
+    float halfScaleY;
 
     //! c‹@
     int life;
+
+    //! ‘¬“x
+    float speed;
+
 };
