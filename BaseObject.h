845552dc0279b6211bd73bcd54fa08f4c1cf32e7
fca406-23 <>
2022-10-04 12:23:25 +0900

    Getterの追加

--- BaseObject.h
+++ BaseObject.h
@@ -9,6 +9,11 @@ public:
     virtual void Update(float deltaTime) = 0;
     virtual void Draw();
 
+    virtual float GetPosX() const { return posX; }
+    virtual float GetPosY() const { return posY; }
+    virtual float GetScaleX() const { return scaleX; }
+    virtual float GetScaleY() const { return scaleY; }
+
 protected:
     //! xç¿ïW
     float posX;
